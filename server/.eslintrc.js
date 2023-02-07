cbb1414dcddd5a6c35a76abfffac4698747ea760
Danil <96256657+OGTutor@users.noreply.github.com>
2023-02-07 11:53:11 +0200

    Implemented file upload n work with static

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,20 +1,24 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        project: 'tsconfig.json',
-        tsconfigRootDir: __dirname,
-        sourceType: 'module',
-    },
-    plugins: ['prettier'],
-    extends: [
-        'plugin:@typescript-eslint/recommended',
-        'plugin:prettier/recommended',
-    ],
-    root: true,
-    env: {
-        node: true,
-        jest: true,
-    },
-    ignorePatterns: ['.eslintrc.js'],
-    rules: { 'no-console': 1, 'prettier/prettier': 0 },
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: 'tsconfig.json',
+		tsconfigRootDir: __dirname,
+		sourceType: 'module',
+	},
+	plugins: ['prettier'],
+	extends: [
+		'plugin:@typescript-eslint/recommended',
+		'plugin:prettier/recommended',
+	],
+	root: true,
+	env: {
+		node: true,
+		jest: true,
+	},
+	ignorePatterns: ['.eslintrc.js'],
+	rules: {
+		'no-console': 1,
+		'prettier/prettier': 0,
+		'@typescript-eslint/no-inferrable-types': 'off',
+	},
 };
