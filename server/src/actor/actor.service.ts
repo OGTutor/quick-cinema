07c8247720c855174476b9dc54045b6592e11be0
Danil <96256657+OGTutor@users.noreply.github.com>
2023-02-10 09:16:14 +0200

    Added the aggregation to actor service

--- server/src/actor/actor.service.ts
+++ server/src/actor/actor.service.ts
@@ -33,9 +33,21 @@ export class ActorService {
 			};
 		}
 
-		return this.ActorModel.find(options)
-			.select('-updatedAt -__v')
-			.sort({ createdAt: 'desc' })
+		return this.ActorModel.aggregate()
+			.match(options)
+			.lookup({
+				from: 'movies',
+				foreignField: 'actors',
+				localField: '_id',
+				as: 'movies',
+			})
+			.addFields({
+				countMovies: {
+					$size: '$movies',
+				},
+			})
+			.project({ __v: 0, updatedAt: 0, movies: 0 })
+			.sort({ createdAt: -1 })
 			.exec();
 	}
 
