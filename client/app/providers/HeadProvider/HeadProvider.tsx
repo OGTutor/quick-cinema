d844e3f0a188eddc207455646803d969117f2952
Danil <96256657+OGTutor@users.noreply.github.com>
2023-02-15 14:41:56 +0200

    Added redux toolkit, toast notifications, nextjs progressbar

---
+++ client/app/providers/HeadProvider/HeadProvider.tsx
@@ -0,0 +1,42 @@
+import Head from 'next/head';
+import NextNProgress from 'nextjs-progressbar';
+import { FC } from 'react';
+
+import { accentColor } from '@/config/constants';
+
+import Favicons from './Favicons';
+
+const HeadProvider: FC<React.PropsWithChildren> = ({ children }) => {
+	return (
+		<>
+			<NextNProgress
+				color={accentColor}
+				startPosition={0.3}
+				stopDelayMs={200}
+				height={3}
+			/>
+			<Head>
+				<meta charSet="UTF-8" />
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1, maximum-scale=1.0"
+				/>
+
+				<Favicons />
+
+				<meta name="theme-color" content={'#181B1E'} />
+				<meta
+					name="msapplication-navbutton-color"
+					content={'#181B1E'}
+				/>
+				<meta
+					name="app-mobile-web-app-status-bar-style"
+					content={'#181B1E'}
+				/>
+			</Head>
+			{children}
+		</>
+	);
+};
+
+export default HeadProvider;
