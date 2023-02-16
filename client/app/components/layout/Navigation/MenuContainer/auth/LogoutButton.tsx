4abed20273a15558d84f4a28391d11e569cc7c6c
Danil <96256657+OGTutor@users.noreply.github.com>
2023-02-16 18:11:51 +0200

    Added authorization n registration with redux. Added general menu for authorized/unauthorized users

--- client/app/components/layout/Navigation/MenuContainer/auth/LogoutButton.tsx
+++ client/app/components/layout/Navigation/MenuContainer/auth/LogoutButton.tsx
@@ -1,7 +1,25 @@
 import { FC } from 'react';
 
+import MaterialIcon from '@/components/ui/MaterialIcon';
+
+import { useActions } from '@/hooks/useActions';
+
 const LogoutButton: FC = () => {
-	return <div>LogoutButton</div>;
+	const { logout } = useActions();
+
+	const handleLogout = (e: React.MouseEvent<HTMLAnchorElement>) => {
+		e.preventDefault();
+		logout();
+	};
+
+	return (
+		<li>
+			<a onClick={handleLogout}>
+				<MaterialIcon name="MdLogout" />
+				<span>Logout</span>
+			</a>
+		</li>
+	);
 };
 
 export default LogoutButton;
--- client/app/components/screens/auth/Auth.tsx
+++ client/app/components/screens/auth/Auth.tsx
@@ -4,6 +4,7 @@ import { SubmitHandler, useForm } from 'react-hook-form';
 import Button from '@/components/ui/form-elements/Button';
 import Heading from '@/components/ui/heading/Heading';
 
+import { useActions } from '@/hooks/useActions';
 import { useAuth } from '@/hooks/useAuth';
 
 import Meta from '@/utils/meta/Meta';
@@ -29,12 +30,7 @@ const Auth: FC = () => {
 		mode: 'onChange',
 	});
 
-	const login = (data: any) => {
-		console.table(data);
-	};
-	const register = (data: any) => {
-		console.table(data);
-	};
+	const { login, register } = useActions();
 
 	const onSubmit: SubmitHandler<IAuthInput> = (data) => {
 		if (type === 'login') login(data);
