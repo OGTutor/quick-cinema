ab06fa559478cfed91929c6b60908cb30c931d88
Danil <96256657+OGTutor@users.noreply.github.com>
2023-02-21 21:48:16 +0200

    Added a table for displaying registered users in the admin panel n the ability to delete them

---
+++ client/app/components/screens/admin/users/useUsers.ts
@@ -0,0 +1,65 @@
+import { ChangeEvent, useMemo, useState } from 'react';
+import { useMutation, useQuery } from 'react-query';
+import { toastr } from 'react-redux-toastr';
+
+import { useDebounce } from '@/hooks/useDebounce';
+
+import { UserService } from '@/services/user.service';
+
+import { convertMongoDate } from '@/utils/date/convertMongoDate';
+import { toastError } from '@/utils/toast-error';
+
+import { getAdminUrl } from '@/config/url.config';
+
+import { ITableItem } from './../../../ui/admin-table/AdminTable/admin-table.interface';
+
+export const useUsers = () => {
+	const [searchTerm, setSearchTerm] = useState('');
+	const debouncedSearch = useDebounce(searchTerm, 500);
+
+	const queryData = useQuery(
+		['user list', debouncedSearch],
+		() => UserService.getAll(debouncedSearch),
+		{
+			select: ({ data }) =>
+				data.map(
+					(user): ITableItem => ({
+						_id: user._id,
+						editUrl: getAdminUrl(`user/edit/${user._id}`),
+						items: [user.email, convertMongoDate(user.createdAt)],
+					})
+				),
+			onError: (error) => {
+				toastError(error, 'User list');
+			},
+		}
+	);
+
+	const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+		setSearchTerm(e.target.value);
+	};
+
+	const { mutateAsync: deleteAsync } = useMutation(
+		'delete user',
+		(userId: string) => UserService.deleteUser(userId),
+		{
+			onError: (error) => {
+				toastError(error, 'Delete user');
+			},
+			onSuccess: () => {
+				toastr.success('Delete user', 'Delete was successful');
+				queryData.refetch();
+			},
+		}
+	);
+
+	return useMemo(
+		() => ({
+			handleSearch,
+			...queryData,
+			searchTerm,
+			deleteAsync,
+		}),
+		[queryData, searchTerm, deleteAsync]
+	);
+};
--- client/pages/manage/users.tsx
+++ client/pages/manage/users.tsx
@@ -1,7 +1,9 @@
+import UserList from '@/components/screens/admin/users/UserList';
+
 import { NextPageAuth } from '@/shared/types/auth.types';
 
 const UserListPage: NextPageAuth = () => {
-	return <div>UserListPage</div>;
+	return <UserList />;
 };
 
 UserListPage.isOnlyAdmin = true;
